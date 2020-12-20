078655d751797ad4a6db3c648c03ba67c67095b4
mrbq <979243959@qq.com>
2020-12-20 12:26:15 +0800

    commit

--- library-web/src/views/reader/QueryReader.vue
+++ library-web/src/views/reader/QueryReader.vue
@@ -1,49 +1,58 @@
 <template>
+
   <div class="queryreader">
-    <div class="text" v-show="true">Now you are <strong>querying</strong> readers, please input the reader's information below.You must input at least one item.</div>
-    <div class="input" v-show="true">
+    <div class="back" v-show="showresult" @click="backtoquery">
+      <span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span>
+      <span>Back to query</span>
+    </div>
+    <div class="text" v-show="!showresult">Now you are <strong>querying</strong> readers, please input the reader's information below.You must input at least one item.</div>
+    <div class="input" v-show="!showresult">
       <div class="form-group cardnum">
-          <label for="cardnum">card number</label>
-          <input type="text" class="form-control" id="cardnum" placeholder="card number" v-model="reader.cardnum" >
+        <label for="cardnum">card number</label>
+        <input type="text" class="form-control" id="cardnum" placeholder="card number" v-model="reader.cardnum" >
       </div>
       <div class="form-group readername">
-          <label for="readername">name</label>
-          <input type="text" class="form-control" id="readername" placeholder="reader's name" v-model="reader.readername"  >
+        <label for="readername">name</label>
+        <input type="text" class="form-control" id="readername" placeholder="reader's name" v-model="reader.readername"  >
       </div>
       <div class="gender">
-          <span>gender </span>
-          <label class="radio-inline">
-            <input type="radio" name="gender" id="male" value="男" v-model="reader.gender" >
-            <img src="@/assets/img/ye.png" alt="ye"> male
-          </label>
-          <label class="radio-inline">
-            <input type="radio" name="gender" id="female" value="女" v-model="reader.gender" >
-            <img src="@/assets/img/girl.png" alt="girl"> female
-          </label>
+        <span>gender </span>
+        <label class="radio-inline">
+          <input type="radio" name="gender" id="male" value="男" v-model="reader.gender" >
+          <img src="@/assets/img/ye.png" alt="ye"> male
+        </label>
+        <label class="radio-inline">
+          <input type="radio" name="gender" id="female" value="女" v-model="reader.gender" >
+          <img src="@/assets/img/girl.png" alt="girl"> female
+        </label>
+        <label class="radio-inline reset" @click="reader.gender = ''">
+          <span class="glyphicon glyphicon-repeat" aria-hidden="true"></span>
+          <span> Reset gender</span>
+        </label>
       </div>
       <div class="form-group department">
-          <label for="department">department</label>
-          <input type="text" class="form-control" id="department" placeholder="department" v-model="reader.department">
+        <label for="department">department</label>
+        <input type="text" class="form-control" id="department" placeholder="department" v-model="reader.department">
       </div>
       <div class="form-group class">
-          <label for="class">class</label>
-          <input type="text" class="form-control" id="class" placeholder="class" v-model="reader.class">
+        <label for="class">class</label>
+        <input type="text" class="form-control" id="class" placeholder="class" v-model="reader.class">
       </div>
       <div class="form-group grade">
-          <label for="grade">grade</label>
-          <input type="text" class="form-control" id="grade" placeholder="grade" v-model="reader.grade">
+        <label for="grade">grade</label>
+        <input type="text" class="form-control" id="grade" placeholder="grade" v-model="reader.grade">
       </div>
       <div class="form-group borrownum">
-          <label for="borrownum">borrow number</label>
-          <input type="text" class="form-control" id="borrownum" placeholder="borrow number" v-model="reader.borrownum">
+        <label for="borrownum">borrow number</label>
+        <input type="text" class="form-control" id="borrownum" placeholder="borrow number" v-model="reader.borrownum">
       </div>
       <div class="query" @click="queryclick">
-        <button  type="button" class="btn btn-primary btn-block" @click="">QUERY</button>
+        <button  type="button" class="btn btn-primary btn-block" >QUERY</button>
       </div>
     </div>
-    <div class="text" v-show="false">Find <strong>{{list.length}}</strong> records !</div>
 
-    <div class="result" v-show="false">
+    <div class="text-result" v-show="showresult" >Find <strong>{{ list && list.length}}</strong> records !</div>
+    <div class="result" v-show="showresult">
       <table class="table table-striped table-hover" v-if="list != null">
         <thead>
         <tr>
@@ -72,6 +81,7 @@
 
       </table>
     </div>
+
     <div class="toast" :class="{'toast-show-warn':warn}" >Please input at least one item！!</div>
     <div class="toast" :class="{'toast-show-querying':querying}" >Querying......</div>
   </div>
@@ -95,18 +105,27 @@ export default {
     },
     warn:false,
     querying:false,
-    list:null
+    list:null,
+    showresult:false
   }
   },
   methods:{
     queryclick(){
       this.querying = true;
       queryreader(this.reader).then(res=>{
-
+        this.list = res
+        setTimeout(()=>{
+          this.querying = false
+          this.showresult = true
+        },1000)
       })
+    },
+    backtoquery(){
+      this.$router.replace('/refresh')
     }
   },
   mounted(){
+    this.$store.commit('changeSubactive','01')
     getallreaders().then(res=>{
       this.list = res
       console.log(this.list);
@@ -119,24 +138,43 @@ export default {
 @fontfamily:-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji;
 .queryreader{
   display: flex;
-  flex-direction: column;
   align-items: center;
+  flex-wrap: wrap;
 }
 .inputareastyle{
   input{
     font-weight: normal;
   }
 }
-.text{
-  width: 80%;
+.back{
+  //background-color: #bbffaa;
+  cursor: pointer;
+  width: 20%;
+  span{
+    margin-left: 10px;
+    font-size: 20px;
+  }
+  span:nth-child(2){
+    font-weight: bold;
+  }
+  &:hover{
+    color: #3498db;
+  }
+}
+.text,.text-result{
+  width: 60%;
   background-color:rgba(26, 188, 156,.8);
   color: #000;
   font-size: 20px;
   padding: 5px 20px;
 }
+.text{
+  width: 80%;
+  margin:0 auto;
+}
 .input{
   padding: 10px 0;
-  margin: 20px 0;
+  margin: 0 auto;
   width: 40%;
   label{
     font-family: @fontfamily;
@@ -158,6 +196,18 @@ export default {
         height: 25px;
       }
     }
+    .reset{
+      padding:0 5px;
+      span{
+        font-size: 15px;
+        font-weight: normal;
+      }
+      &:hover{
+        border: 1px solid #7f8c8d;
+        border-radius: 3px;
+        background-color: #ecf0f1;
+      }
+    }
   }
   .query{
     width: 20%;
@@ -168,6 +218,9 @@ export default {
   width: 100%;
   height: 622px;
   overflow: scroll;
+}
+.result{
+
 }
 
 .toast{
